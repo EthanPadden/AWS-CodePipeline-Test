f4974ac1d7eeeae3abd07c1324987e80d1b28d2e
Ethan Padden <ethanpadden@gmail.com>
2021-03-29 13:55:14 +0100

    Add simple unit tests

--- src/test/java/SimpleTest.java
+++ src/test/java/SimpleTest.java
@@ -1,4 +1,11 @@
-package PACKAGE_NAME;
+import org.junit.Assert;
+import org.junit.jupiter.api.Test;
 
 public class SimpleTest {
+    @Test
+    public void UserTest() {
+        long testId = 123;
+        User user = new User(testId);
+        Assert.assertEquals(testId, user.getId());
+    }
 }
